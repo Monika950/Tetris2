526dac4c00a2dfb57a52e1ec0bdca9268e87785e
Monika Georgieva <monikageorgieva950@gmail.com>
2024-10-22 15:21:21 +0300

    backend wow

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,45 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
 //console.dir(app)
+
+// app.use((req,res) => {
+//     console.log('new request');
+//     //res.send("hello, this is the response");
+//     //res.send({color: 'green'});
+//     res.send("<h1>hello, this is the response</h1>");
+//     });
+
+app.get('/', (req, res) => {
+    res.send('home page');
+})
+
+app.get('/r/:sub', (req, res) => {
+    const {sub} = req.params;
+    res.send(`<h1>${sub}<h1/>`);
+})
+
+app.get('/search', (req, res) => {
+    const {q} = req.query;
+    if(!q) 
+    {
+        res.send(`<h1>nothing searched/<h1/>`);
+    }
+    res.send(`<h1>${q}<h1/>`);
+})
+
+app.get('/cats', (req, res) => {
+    res.send('meow');
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POSTT meow');
+})
+
+app.get('*', (req, res) => {
+    res.send('default I dont know');
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
